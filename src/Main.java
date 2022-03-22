e7dc71c6f36af9297471794645ff30ec7113552b
Admin <s3877746@rmit.edu.vn>
2022-03-22 23:53:21 +0700

    change

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	StudentCollection list = new StudentCollection();
+	list.addStudent(new Student("1", "Thuy", 3.5));
+	list.print();
+    }
+}
--- src/StudentCollection.java
+++ src/StudentCollection.java
@@ -3,9 +3,10 @@ public class StudentCollection {
     int size = 0;
     int MAX_SIZE = 100;
 
-    public StudentCollection(Student[] students) {
+    public StudentCollection() {
         students = new Student[MAX_SIZE];
     }
+
 
     public void addStudent(Student std) {
         for(int i = 0; i < size; i++) {
@@ -22,5 +23,29 @@ public class StudentCollection {
         }
 
         return null;
+    }
+
+    public int rankStudent(String sID) {
+        Student std = null;
+        for(int i = 0; i < size; i++) {
+            if(students[i].id.equals(sID)) {
+                std = students[i];
+            }
+        }
+        if(std == null) return -1;
+
+        int count = 0;
+        double thisGPA = std.gpa;
+        for(int i = 0; i < size; i++) {
+            if(thisGPA - students[i].gpa < 0) count++;
+        }
+        return count + 1;
+    }
+
+    public void print() {
+        for(int i = 0; i < size; i++) {
+            System.out.println("id: " + students[i].id + ", name: " + students[i].name +
+                    ", gpa: " + students[i].gpa);
+        }
     }
 }
